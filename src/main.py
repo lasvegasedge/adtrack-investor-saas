49c7f779332b1e433a31cda6a8ca1f0f1458bf75
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-14 18:22:36 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from src import create_app
+from flask import request, redirect
 
 app = create_app()
+
+@app.before_request
+def redirect_to_www():
+    # Redirect if the host is exactly the root domain
+    if request.host == 'adtrackpitch.online':
+        return redirect(f'https://www.adtrackpitch.online{request.full_path}', code=301)
 
 if __name__ == "__main__":
     app.run(debug=True)
