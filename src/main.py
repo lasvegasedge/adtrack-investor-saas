c0e41f412dd0751094d835b210aa3a90cc06e067
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-14 19:05:23 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
-from flask import request, redirect
-from werkzeug.middleware.proxy_fix import ProxyFix
-
 app = create_app()
-
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
 
 @app.before_request
 def redirect_to_www():
     host = request.host.split(':')[0]
-    print(f"🔹 Host header is: {request.host}")
     if host == 'adtrackpitch.online':
-        target = f'https://www.adtrackpitch.online{request.full_path}'
-        print(f"🌐 Redirecting from {request.host}{request.full_path} to {target}")
-        return redirect(target, code=301)
+        print(f"🌐 Redirecting from {request.host}{request.full_path}")
+        return redirect(f'https://www.adtrackpitch.online{request.full_path}', code=301)
