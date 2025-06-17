ba778d01d48a6e42a83ad1bc2626b8b2504c7273
lasvegasedge <josephsonlineworld@gmail.com>
2025-06-17 14:37:26 -0700

    Update main.py

--- src/routes/main.py
+++ src/routes/main.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('main/index.html')
+
+@main.route('/dashboard')
+def dashboard():
+    return render_template('main/dashboard.html')
