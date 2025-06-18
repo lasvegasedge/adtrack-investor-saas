1bacf21f0c865ffdb537a4bbde4b0fd824515fe8
WestCoastEdge <josephlestrada@gmail.com>
2025-06-18 02:01:30 -0700

    Add homepage route and index template

--- src/routes/main.py
+++ src/routes/main.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+@main.route('/')
+def home():
+    return render_template('main/index.html')
+
 @main.route('/dashboard')
 def dashboard():
     return render_template('main/dashboard.html')
